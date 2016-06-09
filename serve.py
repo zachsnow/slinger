5f1751faa14b0732eb37f28d87af3b7378cb581a
Zach Snow <z@zachsnow.com>
2016-06-09 07:21:25 -0700

    Ignore junk; shebang.

--- serve.py
+++ serve.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python
+
 import BaseHTTPServer, SimpleHTTPServer
 import ssl
 
 httpd = BaseHTTPServer.HTTPServer(('localhost', 4443), SimpleHTTPServer.SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./taut.pem', server_side=True)
+httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./localhost.pem', server_side=True)
 httpd.serve_forever()
