28253872125f1e177eb044504422586c05ad822a
Zach Snow <z@zachsnow.com>
2016-06-09 07:21:06 -0700

    Shortcuts and custom CSS.

--- taut.js
+++ taut.js
@@ -1,3 +1,27 @@
 (function(){
-    alert('taut!');
+  console.log('taut: loaded shim');
+
+  console.log('taut: loading styles');
+  $('<link/>', {
+    rel: 'stylesheet',
+    type: 'text/css',
+    href: '//localhost:4443/taut.css'
+  }).appendTo('head');
+
+  console.log('taut: binding shortcuts');
+
+  var leaveChannel = function(){
+    var model = TS.shared.getActiveModelOb();
+    if (!model){
+      return;
+    }
+    TS.channels.leave(model.id);
+    e.preventDefault();
+  };
+
+  $(window.document).keydown(function(e){
+    if(TS.utility.cmdKey(e) && e.which == 87){
+      leaveChannel();
+    }
+  });
 })();
