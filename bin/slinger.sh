656441f36bf7c56540cfc5a2e4fdf46dfeed80b0
Zach Snow <z@zachsnow.com>
2016-06-11 13:20:06 -0700

    Working on install process.

--- bin/slinger.sh
+++ bin/slinger.sh
@@ -1,11 +1,18 @@
+# Make sure Slack is installed.
+SLACK="/Applications/Slack.app/Contents/MacOS/Slack"
+if [ ! -f $SLACK ];
+then
+   echo "Error: could not find Slack.app"
+fi
+
 # Get latest release.
-URL=`curl -s https://api.github.com/repos/porjo/staticserve/releases/latest | jq --raw-output '.assets[0] | .browser_download_url'`
+URL=`curl -s https://api.github.com/repos/zachsnow/slinger/releases/latest | jq --raw-output '.assets[0] | .browser_download_url'`
 wget ${URL}
 
 # Unzip.
 
 # Patch Slack.
-MD5=`md5 $1`
+MD5=`md5 -q $SLACK`
 PATCH="${MD5}.patch"
 ./patch.py ${PATCH} $1
 
