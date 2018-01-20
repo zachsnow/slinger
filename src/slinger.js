ee61f7811df716ed72d2e925dcdecfe106dce985
Zach Snow <z@zachsnow.com>
2018-01-20 13:56:18 -0800

    Strip more functionality.

--- src/slinger.js
+++ src/slinger.js
@@ -1,10 +1,8 @@
 (function(){
   'use strict';
 
-  var debug = true;
+  var debug = false;
   var version = "0.2.0";
-
-  var stylesUrl = '//zachsnow.github.io/slinger/slinger.css';
 
   // Because sometimes things break and you can't tell if the script
   // is even loading in the Slack app.
@@ -12,138 +10,83 @@
     alert('slinger version ' + version);
   }
 
-  var inApp = false;
-  if(window.TSSSB && window.TSSSB.env){
-    inApp = window.TSSSB.env.mac_ssb_version || window.TSSSB.env.win_ssb_version;
-  }
+  var inApp = window.TSSSB && window.TSSSB.env && window.TSSSB.envdesktop_app_version;
 
   /////////////////////////////////////////////////////////////////////
-  // Logging.
+  // Little link for fun.
   /////////////////////////////////////////////////////////////////////
-  var $slinger = $(
-    '<div id="slinger" style="display: none;">' +
-    '<pre id="log"></pre>' +
-    '<input type="text" id="console" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false">' +
-    '</div>'
-  );
+  var $slinger = $(`
+    <div id="slinger" style="display: none;">
+      <style>
+        #slinger {
+          display: block !important;
+          position: absolute;
+          z-index: 100000;
+          left: 5px;
+          width: 210px;
+          height: 20px;
+          bottom: 5px;
+          background: #78bbe7;
+          border: 1px solid white;
+          color: white;
+          font-family: Monaco,Menlo,Consolas,"Courier New",monospace;
+          font-size: 10px;
+          text-align: center;
+        }
+
+        #slinger a {
+            pointer: cursor;
+            color: white;
+            line-height: 20px;
+            font-weight: bold;
+            text-decoration: none;
+        }
+
+        #slinger:hover a {
+            text-decoration: underline;
+        }
+
+        #channels_scroller {
+            padding-bottom: 50px;
+        }
+      </style>
+      <a href="https://zachsnow.github.io/slinger/">Slinger v${version} &raquo;</a>
+    </div>
+  `);
   $('#slinger').remove();
   $('body').append($slinger);
-
-  var $log = $slinger.find('#log');
-  var $console = $slinger.find('#console');
-
-  var stringify = function(object){
-    if(_.isObject(object) && !(object instanceof Error)){
-      return JSON.stringify(object, null, 2);
-    }
-    return object.toString();
-  };
-
-  var write = function(){
-    if(!$log.length){
-      return;
-    }
-    var text = $log.text();
-    text += _.map(arguments, stringify).join(' ') + '\n';
-    $log.text(text);
-    $log.scrollTop($log[0].scrollHeight);
-  };
-
-  var log = function(){
-    var args = _.toArray(arguments);
-    args.unshift('slinger:');
-    write.apply(this, args);
-  };
-
-  log('loading version ' + version + '...');
-
-  /////////////////////////////////////////////////////////////////////
-  // Console.
-  /////////////////////////////////////////////////////////////////////
-  var commands = [];
-  var currentCommand = 0;
-  $console.on('keypress', function(e){
-    if(e.which === 13){
-      var command = $console.val();
-      $console.val('');
-      var result;
-      try {
-        result = eval(command);
-      }
-      catch(e){
-        result = e;
-      }
-      write('>', _.isUndefined(result) ? '' : result);
-      commands.push(command);
-      currentCommand = commands.length;
-      return;
-    }
-  });
-
-  $console.on('keydown', function(e){
-    if(e.which === 38){
-      currentCommand -= 1;
-      if(currentCommand < 0){
-        currentCommand = commands.length;
-      }
-      $console.val(commands[currentCommand] || '');
-      return;
-    }
-    if(e.which === 40){
-      currentCommand += 1;
-      if(currentCommand > commands.length){
-        currentCommand = 0;
-      }
-      $console.val(commands[currentCommand] || '');
-      return;
-    }
-    if(e.which === 27){
-      currentCommand = commands.length;
-      $console.val('');
-    }
-  });
-
-  /////////////////////////////////////////////////////////////////////
-  // Styles.
-  /////////////////////////////////////////////////////////////////////
-  log('loading styles...');
-  $('<link/>', {
-    rel: 'stylesheet',
-    type: 'text/css',
-    href: stylesUrl
-  }).appendTo('head');
 
   /////////////////////////////////////////////////////////////////////
   // Extra shortcuts.
   /////////////////////////////////////////////////////////////////////
-  log('binding shortcuts...');
+  console.log('slinger: binding shortcuts...');
 
   // leave: leave the current room; like /leave but tries to verify
   // that you actually want to leave in some cases.
   var leave = function(){
     var model = TS.shared.legacyGetActiveModelOb();
     if(!model){
-      log('leave: no active model');
+      console.log('slinger: leave: no active model');
       return;
     }
     if(model.is_im){
-      log('leave: leaving current IM');
+      console.log('slinger: leave: leaving current IM');
       TS.client.ui.maybePromptForClosingIm(model.id)
     }
     else if(model.is_mpim){
-      log('leave: leaving current MPIM');
+      console.log('slinger: leave: leaving current MPIM');
       TS.mpims.closeMpim(model.id);
     }
     else if(model.is_group){
-      log('leave: leaving current group');
+      console.log('slinger: leave: leaving current group');
       TS.client.ui.maybePromptForClosingGroup(model.id);
     }
     else if(model.is_channel){
-      log('leave: leaving current channel');
+      console.log('slinger: leave: leaving current channel');
       TS.channels.leave(model.id);
     }
     else {
-      log('leave: unknown model type', model);
+      console.log('slinger: leave: unknown model type', model);
     }
   };
 
@@ -156,6 +99,8 @@
     }
   };
 
+  // `TS.key_triggers` no longer has a public interface for adding
+  // shortcuts, so we have to hack it in.
   var getFromCode = TS.key_triggers.getFromCode;
   TS.key_triggers.getFromCode = function t(i){
     var ii = TS.interop.i18n.keyCodeEquivalent(i, {useReverseMap:true}).toString();
@@ -163,10 +108,5 @@
     return binding || getFromCode(i);
   };
 
-  log('loaded');
-
-  // Export.
-  window.slinger = {
-    log: log
-  };
+  console.log('slinger: loaded');
 })();
