d8b820ee79ba04daf478d7cd962dcbf3f262364a
Zach Snow <z@zachsnow.com>
2018-01-20 13:22:00 -0800

    New approach to patching.

--- bin/patch.sh
+++ bin/patch.sh
@@ -1,48 +1,46 @@
 #!/usr/bin/env bash
-SLINGER_DIR=`dirname $(dirname $0)`
+SLINGER_DIR=$(cd `dirname $(dirname $0)` && pwd)
 
 # Make sure Slack is installed.
-SLACK="/Applications/Slack.app/Contents/MacOS/Slack"
-if [ ! -f ${SLACK} ];
-then
+echo "slinger: finding Slack.app..."
+SLACK_DIR="/Applications/Slack.app/"
+if [ ! -d ${SLACK_DIR} ]; then
    echo "slinger: could not find Slack.app"
    exit -1
 fi
 
 # Find correct patch.
-SLACK_MD5=`md5 -q ${SLACK}`
-PATCH="${SLINGER_DIR}/patches/${SLACK_MD5}.patch"
-if [ ! -f ${PATCH} ];
-then
-  echo "slinger: no patch file for your version of Slack.app (md5: ${SLACK_MD5})."
-  #exit -1
+echo "slinger: finding patch..."
+RESOURCES="/Applications/Slack.app/Contents/Resources/"
+ASAR="/Applications/Slack.app/Contents/Resources/app.asar"
+ASAR_MD5=`md5 -q ${ASAR}`
+PATCH="${SLINGER_DIR}/patches/${ASAR_MD5}.patch"
+if [ ! -f "${PATCH}" ]; then
+  echo "slinger: no patch file for your version of Slack.app asar (md5: ${ASAR_MD5})."
+  PATCH="/Users/z/Projects/ZachSnow/git/slinger/patches/fad2b241e8882507e4f359bf42f83697.patch"
 fi
-PATCH="${SLINGER_DIR}/patches/7b0bc53e419c44c93a4b9ffee18e114e.patch"
+echo "slinger: found path ${PATCH}"
 
 # Configure patch.
 URL="https://zachsnow.github.io/slinger/slinger.js"
 if [[ "$1" != "" ]]; then
   URL="$1"
-  shift
 fi
+
+# Extract asar.
+echo "slinger: extracting asar..."
+cd "${RESOURCES}" && asar extract "${ASAR}" "${ASAR}.unpacked"
 
 # Patch.
-echo "slinger: patching..."
-python "${SLINGER_DIR}/bin/patch.py" $@ "${PATCH}" "${SLACK}" URL="${URL}"
-if [ $? -ne 0 ];
-then
-  echo "slinger: application of ${SLACK_MD5}.patch failed."
+cd "${ASAR}.unpacked" && bash "${PATCH}" "${URL}"
+if [ $? -ne 0 ]; then
+  echo "slinger: application of ${ASAR_MD5}.patch failed."
   exit -1
 fi
 
-# Re-sign with ad hoc identity.
-echo "Slinger: signing..."
-codesign -f -s - "${SLACK}"
-if [ $? -ne 0 ];
-then
-  echo "slinger: re-signing Slack.app failed."
-  exit -1
-fi
+# Pack asar.
+echo "slinger: packing asar..."
+cd "${RESOURCES}" && asar pack "${ASAR}.unpacked" "${ASAR}"
 
 echo "slinger: done"
 exit 0
